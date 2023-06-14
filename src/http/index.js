5a3a0309c701737a8438804d1f02a2974b04090a
Ekaterina Belova <76008172+kbelovaa@users.noreply.github.com>
2023-06-14 13:30:12 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -8,6 +8,10 @@ const $authHost = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+console.log(1, process.env.REACT_APP_API_URL);
+console.log(2, REACT_APP_API_URL);
+console.log(3, process.env.API_URL);
+
 const authInterceptor = (config) => {
   const authConfig = config;
   authConfig.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
