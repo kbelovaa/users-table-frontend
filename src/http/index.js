f0b030b850dfde2e5e0e27c518a7de41b4aed8b5
Ekaterina Belova <76008172+kbelovaa@users.noreply.github.com>
2023-06-14 15:06:42 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const $host = axios.create({
-  baseURL: "https://users-table-api.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 const $authHost = axios.create({
-  baseURL: "https://users-table-api.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 const authInterceptor = (config) => {
